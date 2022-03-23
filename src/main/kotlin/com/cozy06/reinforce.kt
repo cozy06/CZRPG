ec72207039f3f28eafc8a0ef73d078cc1f9b1183
Wonwoo Jin <jamesjin06@gmail.com>
2022-03-23 23:43:13 +0900

    fourth commit

--- src/main/kotlin/com/cozy06/reinforce.kt
+++ src/main/kotlin/com/cozy06/reinforce.kt
@@ -24,9 +24,7 @@ class reinforce: Listener {
         val action = p.action
         if(action == Action.RIGHT_CLICK_BLOCK) {
             if(p.clickedBlock!!.type == Material.ENCHANTING_TABLE) {
-                player.sendMessage("1")
                 p.isCancelled = true
-                player.sendMessage("2")
                 inv = Bukkit.createInventory(null, 9, "강화")
                 val GLASS_PANE = ItemStack(Material.GRAY_STAINED_GLASS_PANE, 1) //다이아몬드 검 생성
                 val GLASS_PANEMeta = GLASS_PANE.itemMeta //검의 메타데이터
@@ -59,14 +57,18 @@ class reinforce: Listener {
                 val itemMeta = item.itemMeta //검의 메타데이터
                 itemMeta.setDisplayName("강화!") //검의 이름 설정
                 var reinforce = inv!!.getItem(3)?.itemMeta?.lore().toString().length
+                var reinforcenum = (inv!!.getItem(3)?.itemMeta?.lore().toString().length) - 452
+                var reinforceper = 20/reinforcenum
+                var distroyper = -20/((reinforcenum/2)-11)-0.4
+                e.whoClicked.sendMessage("" + reinforceper + "%")
                 val l: MutableList<String> = ArrayList()
-                if(reinforce == null) {
+                if(reinforce == 4) {
                     l.add("강화 확률:" + 100 + "%")
                 }
                 else {
-                    l.add("강화 확률:" + (1/(reinforce*5))*100 + "%")
+                    l.add("강화 확률:" + reinforceper + "%")
                 }
-                l.add("파괴 확률:" + (-20/((reinforce/2)-11)-0.4) + "%")
+                l.add("파괴 확률:" + distroyper + "%")
                 itemMeta.lore = l
                 item.itemMeta = itemMeta //메타데이터 저장
                 inv!!.setItem(8, item)
@@ -85,27 +87,30 @@ class reinforce: Listener {
                     if (reinforcestone != null) {
                         reinforcestone.itemMeta = reinforcestoneMeta
                     } //메타데이터 저장
-                    inv!!.setItem(3, reinforcestone)
+                    inv!!.setItem(5, reinforcestone)
 
-                    var reinforcenum = inv!!.getItem(3)?.itemMeta?.lore().toString().length
-                    player.sendMessage(" "+reinforcenum)
-                    when(if(reinforcenum == 0) {0} else {randomfunc((1/(reinforcenum*5))*100)}){
+                    var reinforcenum1 = inv!!.getItem(3)?.itemMeta?.lore().toString().length
+                    var reinforcenum2 = (inv!!.getItem(3)?.itemMeta?.lore().toString().length) - 452
+                    //player.sendMessage(" "+inv!!.getItem(3)?.itemMeta?.lore().toString())
+                    when(if(reinforcenum1 == 4) {0} else {randomfunc(20/reinforcenum2)}){
                         0 -> {
-                            val item = inv!!.getItem(3)?.type?.let { ItemStack(it, 1) }
+                            player.sendMessage("성공")
+                            val item = inv!!.getItem(3)
+                            //?.type?.let { ItemStack(it, 1) }
                             val itemMeta = item?.itemMeta
 
-                            if(reinforcenum == 0) {
+                            if(reinforcenum1 == 4) {
                                 if (itemMeta != null) {
-                                    itemMeta.lore = listOf("${ChatColor.YELLOW}★")
+                                    itemMeta.lore = listOf("★")
                                 }
-                                else {
-                                    var s = "★"
-                                    for (i in 0..reinforcenum) {
-                                        s = (s + "★")
-                                    }
-                                    if (itemMeta != null) {
-                                        itemMeta.lore = listOf("${ChatColor.YELLOW}" + s)
-                                    }
+                            }
+                            else {
+                                var s = ""
+                                for (i in 0..reinforcenum2) {
+                                    s = (s + "★")
+                                }
+                                if (itemMeta != null) {
+                                    itemMeta.lore = listOf(s)
                                 }
                             }
                             if (item != null) {
@@ -114,10 +119,12 @@ class reinforce: Listener {
                             }
                         }//성공
                         1 -> {
+                            player.sendMessage("실패"+ (-20/((reinforcenum2/2)-11)-0.4))
                             //val distory = (-20/((reinforcenum/2)-11)-0.4).toInt()
-                            when(randomfunc((-20/((reinforcenum/2)-11)-0.4).toInt())) {
-                                0 -> {return}
-                                1 -> {
+                            when(randomfunc((-20/((reinforcenum2/2)-11)-0.4).toInt())) {
+                                1 -> {player.sendMessage("파괴X")}
+                                0 -> {
+                                    player.sendMessage("파괴O")
                                     val air = ItemStack(Material.AIR, 1) //다이아몬드 검 생성
                                     val airMeta = air.itemMeta
                                     inv!!.setItem(3, air)
